c075634d1e5bf88181cdaf1aa2bbd90a732643e1
Ethan Favia <efavia215@live.com>
2016-11-16 15:28:56 -0500

    more setting up of the testing system

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,13 @@
 module.exports = function(config) {
     config.set({
-        
-    })
+        browsers: ['Chrome'],
+        singleRun: true,
+        frameworks: ['mocha'],
+        files: ['app/tests/**/8.tests.jsx'],
+        preprocessors: {
+            'app/tests/**/*.tests.jsx': ['webpack', 'sourcemap']
+        },
+        reporters:['mocha'],
+
+    });
 }

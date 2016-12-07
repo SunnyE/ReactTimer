aba1d7ee740effec6c6e7db8e38ac1096f31f9d0
Ethan Favia <efavia215@live.com>
2016-12-07 12:14:17 -0500

    tests put in for countdownfom.jsx with a spy

--- app/tests/Components/CountdownForm.test.jsx
+++ app/tests/Components/CountdownForm.test.jsx
@@ -6,8 +6,30 @@ var $ = require('jQuery');
 
 var CountdownForm = require('CountdownForm');
 
-describe('COuntdownForm', () => {
+describe('CountdownForm', () => {
     it('should exist', () => {
         expect(CountdownForm).toExist();
-    })
+    });
+
+    it('should call onSetCountdown if valid seconds entered', () => {
+        var spy = expect.createSpy(); 
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(CountdownForm));
+
+        countdownForm.refs.sconds.value='132'; 
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toHaveBeenCalledWith(132);
+    }) 
+
+     it('should not call onSetCountdown if invalid seconds entered', () => {
+        var spy = expect.createSpy(); 
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(CountdownForm));
+
+        countdownForm.refs.seconds.value='asdfasdf'; 
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toNotHaveBeenCalledWith();
+    }) 
 })

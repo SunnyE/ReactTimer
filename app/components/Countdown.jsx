23ee0eb83c62c66f7729c24ad7dbea60ce922f8a
Ethan Favia <efavia215@live.com>
2016-12-07 14:21:28 -0500

    work on the timer and states of the timer

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,11 +5,34 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
     getInitialState: function () {
-        return {count: 0};
+        return {
+            count: 0,
+            countdownStatus: 'stopped'
+        };
     },
+    componentDidUpdate: function (prevProps, prevState) {
+        if(this.state.countdownStatus !== prevState.countdownStatus) {
+            switch(this.state.countdownStatus) {
+                case 'started':
+                this.startTimer(); 
+                break;
+            }
+        }
+    },
+    startTimer: function() {
+        this.timer = setInterval(() => {   
+            var newCount = this.state.count - 1; 
+            this.setState({
+            //      ternary operator used here to set the value of count to either newCount if the number in new count is >= 0 if it isnt then 0 is used.
+            count: newCount >= 0 ? newCount : 0 
+            })
+        },  1000); 
+
+    }, 
     handleSetCountdown: function(seconds) {
         this.setState({
-            count: seconds
+            count: seconds, 
+            countdownStatus: 'started'
         })
     },
     render: function() {

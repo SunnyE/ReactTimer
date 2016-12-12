4134c54680a2bfb6cdc3c472432442e3fd45e169
Ethan Favia <efavia215@live.com>
2016-12-12 11:01:20 -0500

    tests completed for the controls component

--- app/tests/Components/Controls.test.jsx
+++ app/tests/Components/Controls.test.jsx
@@ -3,3 +3,30 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+
+var Controls = require('Controls')
+
+describe('Controls', () => {
+    it('should exist', () => {
+        expect(Controls).toExist();
+    });
+
+    describe('render', () => {
+        it('should render pause when started', () => {
+            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+            var $el = $(ReactDOM.findDOMNode(controls));
+            var $pauseButton = $el.find('button:contains(Pause)');
+
+            //this checks for the amount of buttons found to be 1. If no "contains" was not included then it would have found all buttons
+            expect($pauseButton.length).toBe(1);
+        });
+
+        it('should render Start when paused', () => {
+            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
+            var $el = $(ReactDOM.findDOMNode(controls));
+            var $startButton = $el.find('button:contains(Start)');
+
+            expect($startButton.length).toBe(1);
+        })
+    })
+})
